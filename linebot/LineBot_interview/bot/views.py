a36dfe5090a42c6de8f31160fe552a569cf9153a
JohnnyLin97 <jy.br.lin@gmail.com>
2022-04-20 00:01:16 +0800

    example code 1

---
+++ linebot/LineBot_interview/bot/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+
+# import 必要的函式庫
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+line_bot_api = LineBotApi("Channel_secret")  #在 line_developer取得
+parser = WebhookParser("Channel_access_token") #在 line_developer取得
+
+@csrf_exempt
+def callback(request):
+
+    if request.method == 'POST':
+        
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                line_bot_api.reply_message(
+                    event.reply_token,
+                   TextSendMessage(text=event.message.text)
+                )
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
